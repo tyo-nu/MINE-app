8666268d5860d5e6b08f7ed1afd412d48b244225
James Jeffryes <jamesgjeffryes@gmail.com>
2016-05-25 18:04:02 -0500

    allow operator db setting

--- js/operator.js
+++ js/operator.js
@@ -3,7 +3,14 @@
 angular.module('app').controller('operatorCtl',  function($scope,$stateParams,CompoundDataFactory, sharedFactory) {
     $scope.operatorName = $stateParams.id;
     $scope.img_src = sharedFactory.img_src;
-    var promise = sharedFactory.services.get_operator(sharedFactory.dbId, $stateParams.id);
+    var promise;
+    if (typeof($stateParams.db) != 'undefined') {
+        sharedFactory.setDB($stateParams.db);
+        promise = sharedFactory.services.get_operator($stateParams.db, $stateParams.id);
+    }
+    else {
+        promise = sharedFactory.services.get_operator(sharedFactory.dbId, $stateParams.id);
+    }
     promise.then(
         function (result) {
             $scope.data = result;
@@ -11,7 +18,7 @@ angular.module('app').controller('operatorCtl',  function($scope,$stateParams,Co
             $scope.$apply()
         },
         function (err) {
-            console.log("get_ops fail")
+            console.log("get_operator fail")
         }
     );
 });
