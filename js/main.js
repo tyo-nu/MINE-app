23ee4c3b21a3be626a6d29bf351874ba231357e3
Jon Strutz <jonstrutz11@gmail.com>
2021-07-29 16:14:16 -0500

    forgot to change backend URI back to production

--- js/main.js
+++ js/main.js
@@ -11,8 +11,7 @@ angular.module('app').factory('sharedFactory', function($state, $cookieStore, $r
             'acompound.products', 'acompound.overview'],
         selected_model: "",
         img_src: 'http://localhost:5000/mineserver/get-op-image/',
-        services: new mineDatabaseServices('http://localhost:5000/mineserver/'),
-        //services: new mineDatabaseServices('https://minedatabase.ci.northwestern.edu/mineserver/'),
+        services: new mineDatabaseServices('https://minedatabase.ci.northwestern.edu/mineserver/'),
         numPerPage: 25, // default number of results to show per page
         setDB: function (db_id) {
             console.log("setDB:" + db_id);
